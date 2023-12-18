c86567e91a1b7a1e2cb74125dd6e30b75d2dee64
Jeferson Rocha <31786075+Roccha@users.noreply.github.com>
2023-12-18 18:24:37 -0300

    reparos

---
+++ ex008.js
@@ -0,0 +1,8 @@
+var vel = 100
+console.log(`A velocidade do seu carro é de ${vel}Km/h `)
+if(vel > 60){
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+
+}
+
+console.log('Dirija sempre usando cinto!')
