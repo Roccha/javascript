39dabca2e2d9222265559a1e1e339c4a1d1c11be
Jeferson Rocha <31786075+Roccha@users.noreply.github.com>
2023-12-19 20:47:59 -0300

    console

---
+++ ex010.js
@@ -0,0 +1,8 @@
+var idade = 20
+if (idade <16 ){
+    console.log('não vota!')
+}else if (idade >18 || idade > 65){
+    console.log('Voto Opcional!')
+} else{
+    console.log('Voto Obrigatorio')
+}
