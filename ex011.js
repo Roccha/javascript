1f49171c25d511cbdf346108db7b8fe82b3c751b
Jeferson Rocha <31786075+Roccha@users.noreply.github.com>
2023-12-22 21:17:51 -0300

    ao

---
+++ ex011.js
@@ -0,0 +1,10 @@
+var idade = 18
+if (idade < 16){
+    console.log(`Você tem idade ${idade} anos`)
+    console.log('Não vota')
+}else if( 'idade < 18 || > 65'){ 
+    
+        console.log('Voto opcional')
+} else {
+    console.log('voto obrigatorio')
+}
---
+++ ex013.js
@@ -0,0 +1,12 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+diaSem =6
+/*
+0 -Domingo 
+6 -Sabado */
+
+switch(diaSem){
+
+    case0
+}
+
